e8b71fc1e370293978c12ec299286eaccfd91f2f
Perez Martinez Luis Alberto <perezml2018@dci25-179.ugto.mx>
2018-08-06 12:55:56 -0500

    he modificado el programa

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,3 +1,3 @@
-/*Este es un comentario
-Progrma creado el 06/08/2018 by Alma Gonzalez
+/*Este es un comentario. Me sirve para escribir informacion del programa que estoy haciendo. Por ejemplo este programa imprime la tipica frase " hola mundo a la pantalla"
+Progrma creado el 06/08/2018 por Luis Alberto
 */
