8fe1be87b8b9b329744ef2dd068789f8b27bf1f2
Perez Martinez Luis Alberto <perezml2018@dci25-179.ugto.mx>
2018-08-08 09:24:44 -0500

    Cree y añadi un comentario semana 1

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,3 +1,15 @@
 /*Este es un comentario. Me sirve para escribir informacion del programa que estoy haciendo. Por ejemplo este programa imprime la tipica frase " hola mundo a la pantalla"
-Progrma creado el 06/08/2018 por Luis Alberto
+Progrma creado el 06/08/2018 por Luis Alberto betito
 */
+
+#include<stdio.h>
+
+int main()
+{
+
+  printf("\n HOLA BETO\n");
+  printf("  Este es mi primer programa en la terminal UG\n");
+
+}
+
+/* hasta aqui mi programa */
