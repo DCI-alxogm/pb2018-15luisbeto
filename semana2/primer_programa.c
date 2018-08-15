5fe47c6067f7886a6b026d11ab420c2c50b3d3d7
Perez Martinez Luis Alberto <perezml2018@dci25-179.ugto.mx>
2018-08-15 08:11:49 -0500

    sec creo el segundo programa

---
+++ semana2/primer_programa.c
@@ -0,0 +1,26 @@
+/* este es el primer programa de la segumda semana*/
+#include<stdio.h>
+
+int edad;
+int a;
+int incremento;
+int edadact;
+int i=2028;
+char n[15];
+
+int main()
+{
+    printf ("Hola : ");
+    scanf ("%s", &n);
+    printf ("Primer programa del curso, semana 2\n");
+    printf ("Intruduce el año actual : ");
+    scanf ("%d", &a);
+    printf ("introduce tu edad : ");
+    scanf ("%d", &edad);
+    
+    incremento = a-i;
+    edadact = edad-incremento;
+    
+    printf("%s tu edad en 2028 es : %d",n,edadact);
+    return 0;
+} 
