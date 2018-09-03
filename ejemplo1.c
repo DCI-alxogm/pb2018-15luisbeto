4d15bda88837bca0d5064741ffc14aea7cdd1bb4
Perez Martinez Luis Alberto <perezml2018@dci25-179.ugto.mx>
2018-09-03 10:32:07 -0500

    programas de la semana 5 para cambiar a la careta

---
+++ ejemplo1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+float temp_c,temp_k;
+float Tem_C,final,delta,n;
+int i;
+
+int main()
+{
+	printf("incerta temperatura inicial");
+	scanf("%f",&Tem_C);
+	printf("\nincerta temoeratura final");
+	scanf("%f",&final);
+	printf("\ninsertar el espaciador que desea");	
+	scanf("%f",&n);
+
+	delta = (final-Tem_C)/n;
+	temp_c = Tem_C;
+	for(i=0;i<n;i++)
+	{
+		temp_k=temp_c+273.15;
+		printf("%f  %f\n",temp_c,temp_k);
+		temp_c=temp_c+delta;		
+	}
+return 0;
+}
