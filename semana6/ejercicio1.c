f706348684aab4ba2f0baea00890d79aa823f202
Perez Martinez Luis Alberto <perezml2018@dci25-179.ugto.mx>
2018-09-12 08:42:26 -0500

    ejercicio 1

---
+++ semana6/ejercicio1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int i,N=10;
+float datos[N];
+float
+
+int main()
+{
+for(i=1;i==10;i++)
+{
+printf("cual es tu edad?  ");
+scanf("%f",edad[i]);
+printf("sexo (0.-hombre, 1.-mujer):  ");
+scanf("%f",sexo[i]);
+printf("cual semestre vas(1-9)  ");
+scanf("%f",semestre[i]);
+printf("calificación de la carrera):  ");
+scanf("%f",calificacion[i]);
+}
+}
