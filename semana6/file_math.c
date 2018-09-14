c6d013ce25831447fde1755a6ecb5619486d9918
15luisbeto <15luisbeto.15@gmail.com>
2018-09-14 12:46:27 -0500

    actualización

---
+++ semana6/file_math.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+FILE* data;
+FILE* archivo;
+int final,i,n;
+float e,c,s,l,raiz,delta,x;
+data = fopen("intervalos.txt", "r");
+
+printf("incerta una variable");
+scanf("%f",&x);
+printf("incerta un intervalo");
+scanf("%f",&final);
+printf("incerta el numero de veces que se repite");
+scanf("%d",&n);
+delta=(final)/n;
+
+fclose(data);
+
+archivo = fopen("resultados.txt", "w");
+for(i=0;i<n;i++)
+{
+e=exp(x);
+c=cos(x);
+s=sin(x);
+l=log(x);
+raiz=sqrt(x);
+fprintf(archivo,"\ncos%f \nsen%f \nexp%f \nlog%f \nraiz%f",c,s,e,l,raiz);
+x=x+delta;
+fclose(archivo);
+}
+}
