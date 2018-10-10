2b3958fa94a3418bf816182ccb125206d23ff431
Perez Martinez Luis Alberto <perezml2018@dci25-176.ugto.mx>
2018-10-10 09:52:16 -0500

    semana 9 clase 2

---
+++ ejemplo4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+float cuadrados();
+
+int main(){
+float x,x2;
+printf("introduce un numero\n");
+scanf("%f",&x);
+x2=cuadrados(x);
+printf("El cuadrado de %f es %f\n",x,x2);
+return 0;
+}
+float cuadrados(float h){
+return (h*h);
+}
