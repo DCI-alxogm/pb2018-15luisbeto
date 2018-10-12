6d344b89de6c51720033464f6918210597735480
15luisbeto <42146335+15luisbeto@users.noreply.github.com>
2018-10-12 01:27:23 -0500

    ejemplo3

--- ejemplo3.c
+++ ejemplo3.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 
-void cuadrados(float x,float x2){
-x2=x*x;
-
-	printf("El cuadrado de %f es %f\n",x,x2);
-}
+void cuadrados(float x,float x2);
 
 int main(){
 float x,x2;
@@ -12,5 +8,10 @@ printf("introduce un numero\n");
 scanf("%f",&x);
 cuadrados(x,x2);
 return 0;
+}
+void cuadrados(float x,float x2){
+x2=x*x;
+
+	printf("El cuadrado de %f es %f\n",x,x2);
 }
 
